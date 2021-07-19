bd724ce853131443d02c159923cbd7e44cba982e
WillW34 <78357757+WillW34@users.noreply.github.com>
2021-07-19 15:39:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, abort, send_file, jsonify
+from flask import Flask, request, abort, send_file, jsonify, send_from_directory
 import os, shutil, glob, random, string, tempfile, requests
 import sys
 sys.path.insert(0,'shortbol')
@@ -27,7 +27,7 @@ def evaluate():
         file_name = file['filename']
         file_type = file['type']
         file_url = file['url']
-        
+
         ########## REPLACE THIS SECTION WITH OWN RUN CODE #################
         ##IN THE SPECIAL CASE THAT THE EXTENSION HAS NO MIME TYPE USE SOMETHING LIKE THIS
         file_type = file_name.split('.')[-1]
@@ -44,7 +44,7 @@ def evaluate():
         
         file_type_acceptable = file_type in acceptable_types
         file_type_useable = file_type in useful_types
-        print(file_type_acceptable,file_type_useable)
+        #print(file_type_acceptable,file_type_useable)
         
         ################## END SECTION ####################################
         
@@ -71,7 +71,7 @@ def run():
     #take in run manifest
     run_manifest = request.get_json(force=True)
     files = run_manifest['manifest']['files']
-        
+
     #initiate response manifest
     run_response_manifest = {"results":[]}
 
@@ -80,21 +80,24 @@ def run():
         file_type = a_file['type']
         file_url = a_file['url']
         data = str(a_file)
-        
+
         converted_file_name = file_name + ".converted"
         file_path_out = os.path.join(zip_in_dir_name, converted_file_name)
-        
+
         ########## REPLACE THIS SECTION WITH OWN RUN CODE #################
-        
+
         #Retrieve file from manifest
         run_data = requests.get(file_url)
+
+        # return data, run_data.text
+
         sbh_input = os.path.join(temp_dir.name,"temp_shb.txt")
         with open(sbh_input,"w") as sbh_file:
             sbh_file.write(run_data.text)
-            shb_run.parse_from_file(sbh_file.name, out=file_path_out, optpaths=[shortbol_libs])
+        shb_run.parse_from_file(sbh_file.name, out=file_path_out, optpaths=[shortbol_libs])
 
         ################## END SECTION ####################################
-    
+
         # add name of converted file to manifest
         run_response_manifest["results"].append({"filename":converted_file_name,
                                 "sources":[file_name]})
@@ -105,13 +108,27 @@ def run():
     #create manifest file
     file_path_out = os.path.join(zip_in_dir_name, "manifest.json")
     with open(file_path_out, 'w') as manifest_file:
-            manifest_file.write(str(run_response_manifest)) 
-      
+            manifest_file.write(str(run_response_manifest))
+
     with tempfile.NamedTemporaryFile() as temp_file:
         #create zip file of converted files and manifest
         shutil.make_archive(temp_file.name, 'zip', zip_in_dir_name)
-        
+
         #delete zip in directory
-        #shutil.rmtree(zip_in_dir_name)
+        shutil.rmtree(zip_in_dir_name)
         #return zip file
         return send_file(temp_file.name + ".zip")
+
+
+@app.route("/testing/<file_name>")
+def success(file_name):
+    cwd = os.getcwd()
+    path = os.path.join(cwd, 'testing')
+    try:
+        return send_from_directory(path, file_name)
+    except:
+        with open(os.path.join(cwd, "Static_File_Not_Found.html")) as file:
+            error_message = file.read()
+
+        error_message = error_message.replace('REPLACE_FILENAME', file_name)
+        return error_message, 404
