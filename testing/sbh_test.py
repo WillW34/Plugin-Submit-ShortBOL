26397838687eae5bc44bd798278df6a6cedab948
WillW34 <78357757+WillW34@users.noreply.github.com>
2021-07-19 15:39:30 +0100

    Update sbh_test.py

--- testing/sbh_test.py
+++ testing/sbh_test.py
@@ -1,11 +1,11 @@
 import requests
 import urllib
 
-eval_url = "http://127.0.0.1:8080/evaluate"
-run_url = "http://127.0.0.1:8080/run"
+eval_url = "http://localhost:5000/evaluate"
+run_url = "http://localhost:5000/run"
 
 manifest = """{"manifest": 
-                {"files": [{"url": "http://localhost:5000/public/plugintest.txt", "filename": "plugintest.txt", "type": "text/plain"}]}}"""
+                {"files": [{"url": "http://localhost:5000/testing/plugintest.txt", "filename": "plugintest.txt", "type": "text/plain"}]}}"""
 
 import requests, zipfile, io
 
@@ -14,7 +14,7 @@ def test():
     eval_response = requests.post(eval_url, data=manifest)
     run_response = requests.post(run_url, data=manifest)
 
-    # print(eval_response.text)
+    print(eval_response.text)
     z = zipfile.ZipFile(io.BytesIO(run_response.content))
     z.extractall("out")
 
